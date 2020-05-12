e92a4d99f657fa0700f693a4e0fdaf428784feee
mkemalsimsek <62071572+mkemalsimsek@users.noreply.github.com>
2020-05-12 16:07:20 +0100

    Update Practice.java

--- src/tests/Practice.java
+++ src/tests/Practice.java
@@ -3,5 +3,8 @@ package tests;
 public class Practice {
     public static void main(String[] args) {
         System.out.println("This is Practice class");
+        
+        
+        //This line written from github
     }
 }
